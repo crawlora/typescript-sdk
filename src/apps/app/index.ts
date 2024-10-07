4f56456921245cfe4360ccc6bbc79044cbbcb36a
dharmendrasha <dharmendra.soni@scaletech.xyz>
2024-10-07 14:58:16 +0530

    fixed: app upload with files

--- src/apps/app/index.ts
+++ src/apps/app/index.ts
@@ -1,18 +1,24 @@
+import FormData from "form-data";
 import { Crawlora } from "../../main";
 import { CreateAppType, UpdateAppType } from "./types";
+import { createReadStream } from "fs";
 
 export class Application extends Crawlora {
   constructor(apiKey?: string, baseUrl?: URL) {
     super(apiKey, baseUrl);
   }
 
-  async get(page: number = 1, recordPerPage: number = 10, order: string = 'desc') {
+  async get(
+    page: number = 1,
+    recordPerPage: number = 10,
+    order: string = "desc"
+  ) {
     const { data } = await this.api().get(`/application`, {
       params: {
         page,
         recordPerPage,
-        order
-      }
+        order,
+      },
     });
     return data;
   }
@@ -23,18 +29,71 @@ export class Application extends Crawlora {
   }
 
   async create(body: CreateAppType) {
-    const { data } = await this.api().post(`/application`, body)
+    const form = new FormData();
+    form.append("title", body.title);
+    form.append("description", body.description);
+    form.append("file", createReadStream(body.file_path));
+    form.append("icon", createReadStream(body.icon));
+    form.append("banner", createReadStream(body.banner));
+    form.append("author", body.author);
+    form.append("version", body.version);
+
+    // Append each input item
+    body.input.forEach((input) => {
+      form.append("input[]", JSON.stringify(input));
+    });
+
+    body.screenshots.forEach((inp) => {
+      form.append("screenshots[]", createReadStream(inp));
+    });
+
+    const { data } = await this.api().post(`/application`, form, {
+      headers: {
+        ...form.getHeaders(),
+      },
+    });
     return data;
   }
 
   async update(id: string, body: UpdateAppType) {
-    const { data } = await this.api().put(`/application/${id}`, body)
-    return data
+    const form = new FormData();
+    form.append("title", body.title);
+    form.append("description", body.description);
+    if(body.file_path){
+      form.append("file", createReadStream(body.file_path));
+    }
+
+    if(body.icon){
+      form.append("icon", createReadStream(body.icon));
+    }
+
+    if(body.banner){
+      form.append("banner", createReadStream(body.banner));
+    }
+
+    
+    form.append("author", body.author);
+    form.append("version", body.version);
+
+    // Append each input item
+    body?.input?.forEach((input) => {
+      form.append("input[]", JSON.stringify(input));
+    });
+
+    body?.screenshots?.forEach((inp) => {
+      form.append("screenshots[]", createReadStream(inp));
+    });
+
+    const { data } = await this.api().put(`/application/${id}`, body, {
+      headers: {
+        ...form.getHeaders()
+      }
+    });
+    return data;
   }
 
   async delete(id: string) {
-    const { data } = await this.api().delete(`/application/${id}`)
-    return data
+    const { data } = await this.api().delete(`/application/${id}`);
+    return data;
   }
-
 }
